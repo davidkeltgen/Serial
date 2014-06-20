81045b8501d55c5a3f99aa57a14c276a2e8a6384
david <david@david-HP-620>
2014-06-20 08:46:19 -0600

    First commit.

---
+++ SerialTesting_App/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: david
+ *
+ * Created on June 20, 2014, 8:40 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
